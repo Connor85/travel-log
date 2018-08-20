1d366bcf94c53e53a8af7cac714060aa7c2c7fe5
Akjol Jaenbai and Connor McCarthy <akjol@>
2018-08-20 09:42:48 -0700

    add JS, HTML, css. site is working

--- travel/js/scipts.js
+++ travel/js/scipts.js
@@ -1,1 +1,36 @@
+// business logic
+function WheelsUp (location, landmark, time) {
+  this.locationName = location;
+  this.landmarkName = landmark;
+  this.timeOfYear = time;
+}
+// WheelsUp.prototype.location = function(){
+//   return this.location
+// }
+//user interface logic
 
+$(document).ready(function(){
+  $("form#new-travel-location").submit(function(event){
+    event.preventDefault();
+    var inputtedLocation = $("input#new-location").val();
+    var inputtedLandmark = $("input#new-landmark").val();
+    var inputtedTime = $("input#new-time").val();
+
+    var newWheelsUp = new WheelsUp (inputtedLocation, inputtedLandmark, inputtedTime);
+
+    $("ul#places").append("<li><span class='place'>" + newWheelsUp.locationName + "</span></li>")
+
+    $(".place").last().click(function(){
+      $("#show-places").show();
+      $("#show-places h2").text(newWheelsUp.locationName);      $(".locations").text(newWheelsUp.locationName);
+      $(".landmarks").text(newWheelsUp.landmarkName);
+      $(".times-of-year").text(newWheelsUp.timeOfYear);
+
+    });
+
+    $("input#new-location").val("");
+    $("input#new-landmark").val("");
+    $("input#new-time").val("");
+
+  });
+});
